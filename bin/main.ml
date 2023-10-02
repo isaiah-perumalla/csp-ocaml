51d24d3a21a9232198f95de9750fbf9f18110d08
isaiah-p <0x7bb@protonmail.com>
2023-10-02 17:41:05 +0100

    check point

--- bin/main.ml
+++ bin/main.ml
@@ -1,28 +1,50 @@
 open Core
+open Garden
 
-let vacant line_num line =
-  let _, vacant_spots = String.fold line ~init:(0, []) ~f:(fun (x,acc) ch -> 
+exception InvalidInput of string
+
+let blocked_spots line_num line =
+  let _, blocked = String.fold line ~init:(0, []) ~f:(fun (x,acc) ch -> 
     let x' = x +1 in
-    if (phys_equal ch  'X') then (x',acc) 
+    if (phys_equal ch  ' ') then (x',acc) 
     else (x', (x, line_num)::acc)) in
-    vacant_spots
+    blocked
 
 let parse file =   
   let first_line = Option.value_exn (In_channel.input_line file) in 
-  let init = vacant 0 first_line in
-  let height, empty_spots = In_channel.fold_lines file ~init: (1, init) 
+  let init = blocked_spots 0 first_line in
+  let height, blocked = In_channel.fold_lines file ~init: (1, init) 
                             ~f: (fun (number, acc) line ->
-      let empty_spots = List.rev_append acc (vacant number line) in
-      (number+1, empty_spots)) in
-  height,empty_spots, (String.length first_line)
+      let blocked = List.rev_append acc (blocked_spots number line) in
+      (number+1, blocked)) in
+  height,blocked, (String.length first_line)
 
-  
-let run garden_file _  = 
+let parse_constraints f = 
+  In_channel.fold_lines f ~init:[] ~f: (fun acc line -> 
+    match (String.split line ~on: ',') with 
+    |[f;qty;dist] -> ((String.get f 0), (int_of_string qty), (int_of_string dist))::acc
+    |_ -> raise (InvalidInput line))
+
+let run garden_file flower_file  = 
   In_channel.with_file garden_file ~f: (fun file -> 
-   let height, empty_spots, width  = parse file in 
+   let height, blocked, width  = parse file in
+   let constraints = In_channel.with_file flower_file ~f: (fun file ->
+    parse_constraints file) in  
+   let module MyGarden = MakeGarden(
+    struct
+      let height = height
+      let width = width
+      let blocked = blocked
+  end) in
+  let g = MyGarden.empty in
+  Printf.printf "%s" (MyGarden.to_string g);
+  
    let fmt = format_of_string  "height=%i width=%i\n" in 
-   List.iter empty_spots ~f: (fun (x,y) -> printf "(%d,%d)\n" x y);
-   Printf.printf fmt height width) 
+   (* List.iter blocked ~f: (fun (x,y) -> printf "(%d,%d)\n" x y); *)
+   List.iter constraints ~f: (fun (f,qty,dist) -> printf "%c, qty=%i, dist=%i \n" f qty dist);
+   Printf.printf fmt height width)
+    
+    
 
 
 let command =
--- lib/garden.ml
+++ lib/garden.ml
@@ -1,14 +1,94 @@
+module type Layout = sig
+  type pos = (int*int)
+  
+  type t
+  type flower  
+  val distance: pos -> pos -> int
 
-type pos = (int * int)
+  val vacant: pos list
+  val is_blocked:  pos -> bool
 
-type t = { bottom_right: pos}
+  val plant: t -> flower -> pos -> (t,unit) Result.t  
+  
+  val empty: t
 
-let distance a b = 
-  let (x0,y0) = a in
-  let (x1, y1) = b in
-  (abs (x1-x0)) + (abs (y0 -y1)) 
+  val to_string: t -> string
 
-let create width height  = { bottom_right = (width, height)}
+end
 
-let width = function
-|{bottom_right = (x, _)} -> x 
+module type Dimension = sig
+  val height : int
+  val width : int
+  val blocked: (int*int) list
+end
+
+module MakeGarden(Dim : Dimension) : Layout = struct
+  module PosOrd = struct
+    type t = (int * int)
+  let compare (x0,y0) (x1, y1)= 
+    match (compare y0 y1) with
+      |0 -> compare x0 x1
+      |r -> r
+
+end
+  module PositionSet = Set.Make(PosOrd)
+  module PositionMap = Map.Make(PosOrd)
+
+  let height = Dim.height
+  let width = Dim.width
+  let blocked = PositionSet.of_list Dim.blocked
+
+  let is_blocked (x,y) = 
+    (x < 0 ) || (y < 0) || (x >= width) || (y >= height) || (PositionSet.mem (x,y) blocked)    
+
+
+  let all_pos = PositionSet.of_list (List.concat 
+      (List.init height (fun y ->
+        List.init width (fun x -> (x,y))))) 
+  
+  
+  let vacant = 
+    let all = (PositionSet.filter (fun pos -> not (is_blocked pos)) all_pos) in
+    PositionSet.fold (fun p acc -> p::acc) all []
+
+  
+  type pos = (int * int)
+
+  
+  type flower = char
+
+  type t = { flowers: flower PositionMap.t }
+  
+  
+  let empty = {flowers = PositionMap.empty }
+  
+  let plant t f pos = 
+      if is_blocked pos then Error () 
+      else if (PositionMap.mem pos t.flowers) then Error ()
+      else Ok({flowers = (PositionMap.add pos f t.flowers)})
+  
+  let distance a b = 
+    let (x0,y0) = a in
+    let (x1, y1) = b in
+    (abs (x1-x0)) + (abs (y0 -y1))
+    
+  let to_string t = 
+    let buf = Buffer.create (height * width) in
+    (* List.iter (fun (x,y) -> 
+      if x  = 0 then Buffer.add_char buf '\n';
+      let ch = match PositionMap.find_opt (x,y) t.flowers with
+              |Some(f) ->  f
+              |_ -> if (is_blocked (x,y)) then 'X' else ' ' in
+      Buffer.add_char buf ch) all_pos;
+       *)
+       PositionSet.iter (
+        fun (x,y) -> 
+          let ch = match PositionMap.find_opt (x,y) t.flowers with
+              |Some(f) ->  f
+              |_ -> if (is_blocked (x,y)) then 'X' else ' ' in
+            Buffer.add_char buf ch;
+        if x == (width -1) then Buffer.add_string buf "\n"
+        ) all_pos;
+    Buffer.contents buf
+
+end
